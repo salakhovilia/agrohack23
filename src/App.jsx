8832e6a83d27d2d4c95f41178170b641b6fa5aac
salakhovilia <salahov.ilia24@gmail.com>
2023-11-18 10:00:19 +0400

    init layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
+import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar className={'bg-indigo-600'} isBlurred={true} position={'sticky'}>
+        <NavbarContent>
+          <NavbarBrand>
+            <p className="font-bold text-white">Agrocode</p>
+          </NavbarBrand>
+        </NavbarContent>
+      </Navbar>
+      <main className="h-full w-full bg-indigo-950">
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Index from './pages/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Index />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </RouterProvider>
   </React.StrictMode>,
 );
